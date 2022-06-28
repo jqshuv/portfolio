9cee7949014ed09dcf702b16311742ab0bb4eda5
Joshua Schmitt <jqshuv@gmail.com>
2022-06-28 19:41:27 +0000

    fix: :bug: fix global site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Joshua Schmitt',
+    title: 'Joshua Schmitt | Portfolio',
     htmlAttrs: {
       lang: 'en',
     },
